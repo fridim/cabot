45b3db457b1ea55cbefa3c7ab69d09a09cfdf099
Guillaume Coré <fridim@onfi.re>
2017-01-31 13:49:29 +0100

    add quote.rb plugin

---
+++ plugins_examples/quote.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+STDOUT.sync = true
+
+$db = "quotes.txt"
+if !File.exists? $db
+  system("touch #{$db}")
+end
+
+def getquote()
+  return %x{shuf -n 1 #{$db}}
+end
+
+def setquote(quote)
+  return system("echo \"#{quote}\" >> #{$db}")
+end
+
+STDIN.each_line do |l| 
+  if l =~ /^:[^ ]+ PRIVMSG (#[^ ]+) :(:.*)\r\n$/
+    channel = $1
+    what = $2
+
+    if what =~ /^:setquote (.*)/
+      if setquote($1)
+        puts "PRIVMSG #{channel} :quote saved"
+      end
+
+    elsif what =~ /^:(get)?quote/
+      quote =  getquote()
+      puts "PRIVMSG #{channel} :#{quote}" if quote.length > 0
+    end
+  end
+end
