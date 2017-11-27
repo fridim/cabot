6c506ba13835d6a4b24f92297a3f8b8a27614a00
Pierre-Alain TORET <pierre-alain.toret@protonmail.com>
2017-11-27 13:44:44 +0100

    fix logic in archive_urls.go

--- plugins_examples/archive_urls/archive_urls.go
+++ plugins_examples/archive_urls/archive_urls.go
@@ -9,42 +9,54 @@ import (
 )
 
 func main() {
+
+	destFile := "./urls.txt"
+
+	blacklistMatch := [2]string{
+		"",
+		"./urls.txt",
+	}
+
 	s := bufio.NewScanner(os.Stdin) 
 	for s.Scan() {
 		line := s.Text()
 		if strings.Contains(line, "PRIVMSG") == true {
 			skip := false;
-
-	    		re := regexp.MustCompile("https?\\:\\/\\/[[:^space:]]*")
-	    		match := re.FindString(line)
-
-			if match != "" && match != "https://arch.cccp.io/urls.txt" {
-	    			fr, err := os.Open("/var/www/html/arch/urls.txt")
-	    			if err != nil {
-				    fmt.Fprintln(os.Stderr, "error", err)
-	    			}
-				scanner := bufio.NewScanner(fr)
-  				for scanner.Scan() {
-					if strings.Contains(scanner.Text(), match) {
-						skip = true
-						fmt.Fprintf(os.Stdout, "PRIVMSG #esil :url déjà enregistrée\r\n")	
-					}
-  				}
-				
-				if skip == false {
-	    				f, err := os.OpenFile("/var/www/html/arch/urls.txt", os.O_APPEND|os.O_WRONLY, 0600)
-
-	    				if err != nil {
-					    fmt.Fprintln(os.Stderr, "error", err)
-	    				}
-
-	    				if _, err = f.WriteString(match + "\n"); err != nil {
-					    fmt.Fprintln(os.Stderr, "error", err)
-	    				}
-					f.Close()
+			
+			re := regexp.MustCompile("https?\\:\\/\\/[[:^space:]]*")
+			match := re.FindString(line)
+			
+			for i := 1; i < len(blacklistMatch); i++ {
+				if match == blacklistMatch[i] {
+					skip = true
 				}
-
-				fr.Close()
+			}
+			
+			fr, err := os.Open(destFile)
+			if err != nil {
+			    fmt.Fprintln(os.Stderr, "error", err)
+			}
+			scanner := bufio.NewScanner(fr)
+			for scanner.Scan() {
+				if strings.Contains(scanner.Text(), match) {
+					skip = true
+				}
+			}
+			fr.Close()
+			
+			if skip == false {
+				
+				f, err := os.OpenFile(destFile, os.O_APPEND|os.O_WRONLY, 0600)
+			
+				if err != nil {
+				    fmt.Fprintln(os.Stderr, "error", err)
+				}
+			
+				if _, err = f.WriteString(match + "\n"); err != nil {
+				    fmt.Fprintln(os.Stderr, "error", err)
+				}
+				f.Close()
+			
 			}
 		}
 	}
