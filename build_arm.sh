b88ed3d43b45a7d6dcd5cf0ba66fa3ce95d3f219
Guillaume Coré <fridim@onfi.re>
2023-03-04 14:28:16 +0100

    Add plugins for OpenAI Chat
    Cabot will reply using openAI chat conversation.
    - 15 messages history for now
    - 100 tokens max reply
    - temperature, presencePenalty, FrequencePenalty of 0.8

--- build_arm.sh
+++ build_arm.sh
@@ -1,8 +1,10 @@
-set -e
+#!/bin/bash
+set -xe
 
-GOOS=linux GOARCH=arm64 go build *.go -o cabot_linux_arm64
+GOOS=linux GOARCH=arm64 go build -o cabot_linux_arm64 *.go
 
-for i in feed ping karma weather; do
+for i in feed ping karma weather chatgpt; do
+	echo $i
 	(
 	cd plugins_examples/$i
 	GOOS=linux GOARCH=arm64 go build -o ../../plugins/${i}_linux_arm64 ${i}.go
---
+++ plugins_examples/chatgpt/chatgpt.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	gogpt "github.com/sashabaranov/go-gpt3"
+	"os"
+	"regexp"
+	"bufio"
+	"strings"
+)
+
+var r1 = regexp.MustCompile(":([^!]+)![^ ]+ PRIVMSG (#[^ ]+) :(.*)")
+var r2 = regexp.MustCompile("^cabot[,:]? (.*)")
+
+func parsePrompt(line string) (gogpt.ChatCompletionMessage, string, bool) {
+	m := r1.FindStringSubmatch(line)
+	if m == nil {
+		return gogpt.ChatCompletionMessage{}, "", false
+	}
+
+	nick := m[1]
+	channel := m[2]
+	text := m[3]
+	prompt := ""
+
+	if m2 := r2.FindStringSubmatch(text); m2 != nil {
+		prompt = m2[1]
+
+		return gogpt.ChatCompletionMessage{
+			Role: "user",
+			Content: fmt.Sprintf("C'est %s qui parle. %s", nick, prompt),
+		},
+			channel,
+			true
+	}
+
+	return gogpt.ChatCompletionMessage{}, "", false
+}
+
+var messages = make(map[string][]gogpt.ChatCompletionMessage)
+var messagesInit = []gogpt.ChatCompletionMessage{
+	{
+		Role: "system",
+		Content: "Tu réponds toujours sous la forme d'une réponse très courte, moins de 80 caractères. Prends le temps de développer point par point ton raisonnement, mais n'inclus dans ta réponse que la conclusion.",
+	},
+}
+
+var maxMessages = 15
+
+func main() {
+	tokenBytes, err := os.ReadFile("openapi_token.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading token.txt: %v", err)
+		os.Exit(1)
+	}
+
+	token := strings.Trim(string(tokenBytes), " \t\r\n")
+	c := gogpt.NewClient(token)
+	bio := bufio.NewReader(os.Stdin)
+	ctx := context.Background()
+
+	for {
+		line, err := bio.ReadString('\n')
+		if err == nil {
+			if message, channel, ok := parsePrompt(line); ok {
+				if _, ok := messages[channel]; !ok {
+					messages[channel] = make([]gogpt.ChatCompletionMessage, 0)
+				}
+				messages[channel] = append(messages[channel], message)
+				// Limit the number of messages to 10
+				if len(messages[channel]) > maxMessages {
+					messages[channel] = messages[channel][1:]
+				}
+
+				// Call the GPT-3 API
+				req := gogpt.ChatCompletionRequest{
+					Model:     gogpt.GPT3Dot5Turbo,
+					MaxTokens: 100,
+					Temperature: 0.8,
+					PresencePenalty: 0.8,
+					FrequencyPenalty: 0.8,
+					Messages: append(messagesInit, messages[channel]...),
+				}
+				resp, err := c.CreateChatCompletion(ctx, req)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error calling GPT-3 API: %v", err)
+					continue
+				}
+
+				if len(resp.Choices) > 0 {
+					for _, c := range resp.Choices {
+						messages[channel] = append(messages[channel], c.Message)
+						fmt.Printf(
+							"PRIVMSG %s :%s\n",
+							channel,
+							c.Message.Content)
+					}
+				}
+			}
+		}
+	}
+}
